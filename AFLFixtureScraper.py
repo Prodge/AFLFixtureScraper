31edc05d1c2143d38af73644abfa8d661f7efa92
Prodge <prodge@prodge.net>
2016-03-31 00:23:13 +0800

    Updated for 2016

--- AFLFixtureScraper.py
+++ AFLFixtureScraper.py
@@ -5,7 +5,7 @@ Author: Prodge
 
 Scrapes the afl.com.au website for fixtures for a given season
 
-nextGame() Moves to the start of a game listing, 
+nextGame() Moves to the start of a game listing,
 getHome() Returns the next instance of a home team
 getAway() Returns the next instance of an away team
 
@@ -26,25 +26,29 @@ def nextGame():
     for i in range(len(html)):
         if html[i:i+len(gameDiv)] == gameDiv:
             html = html[i+len(gameDiv):]
-            return True 
+            return True
         if html[i:i+len(end)] == end:
             return False
 
 def getHome():
     global html
-    home = '<span class="home"><span class="team">'
+    home = 'home"><span class="team">'
     for i in range(len(html)):
         if html[i:i+len(home)] == home:
             html = html[i+len(home):]
             break
-    end = ' v</span>'
+    end = '</span>'
     for j in range(len(html)):
         if html[j:j+len(end)] == end:
-            return html[:j]
+            home = html[:j]
+            verses_text = ' v'
+            if home.endswith(verses_text):
+                home = home[:-len(verses_text)]
+            return home
 
 def getAway():
     global html
-    home = '<span class="away"><span class="team">'
+    home = 'away"><span class="team">'
     for i in range(len(html)):
         if html[i:i+len(home)] == home:
             html = html[i+len(home):]
@@ -58,7 +62,7 @@ def main():
     global html
     maxNumGames = 9
     numRounds = 23
-    year = 2015
+    year = 2016
     for x in range(numRounds):
         url = "http://www.afl.com.au/fixture?roundId=CD_R" + str(year) + "014" + str("%02d" % (x+1)) + "#tround"
         page = urllib2.urlopen(url)
@@ -66,9 +70,9 @@ def main():
         print('\n\nRound ' + str(x+1) + ':')
         for i in range(maxNumGames):
             if not nextGame():
-                continue 
+                continue
             home = getHome()
-            away = getAway()            
+            away = getAway()
             print('Game: ' + str(i+1) + '   Home: ' + home + '\n          Away: ' + away)
 
 main()
